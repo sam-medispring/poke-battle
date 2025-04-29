97b15c2947d1ab469618f922ab0c33b2218770d5
LAPSAM6\sjocq <sam.jocque@medispring.be>
2025-04-29 15:12:34 +0200

    test

---
+++ libs/features/poke-detail/poke-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-detail',
+  imports: [],
+  templateUrl: './poke-detail.component.html',
+  styleUrl: './poke-detail.component.css'
+})
+export class PokeDetailComponent {
+
+}
